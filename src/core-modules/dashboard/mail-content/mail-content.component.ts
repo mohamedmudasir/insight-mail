ec4e1c936bf7c5ab9f3685eb7f0874ab48c5709d
Mohamed Mudasir <mohamedmudasir00@gmail.com>
2019-12-22 23:40:22 +0530

    mail-compose changes

--- src/core-modules/dashboard/mail-content/mail-content.component.ts
+++ src/core-modules/dashboard/mail-content/mail-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mail-content',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mail-content.component.scss']
 })
 export class MailContentComponent implements OnInit {
-
+  @Input() mail: Object;
   constructor() { }
 
   ngOnInit() {
